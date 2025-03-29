528a48b67a7a18856449e9457f01acbf666820e1
Kyron <tfkyron@gmail.com>
2025-03-29 17:43:08 +0000

    Fix misplaced code

--- wp-content/themes/beyond-trans/footer.php
+++ wp-content/themes/beyond-trans/footer.php
@@ -1,15 +1,10 @@
-<!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+<footer>
+    <div class="container">
+        <?php get_template_part('template-parts/common/logo'); ?>
+        <?php get_template_part('template-parts/layout/menus/footer', 'menu'); ?>
+        <?php wp_footer(); ?>
+    </div>
+</footer>
+</body>
 
-<head>
-    <meta charset="<?php bloginfo('charset'); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="<?php echo get_bloginfo('description'); ?>">
-    <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" />
-    <title><?php wp_title('|', true, 'right'); ?><?php bloginfo('name'); ?></title>
-    <?php get_template_part('template-parts/common/favicon'); ?>
-    <?php wp_head(); ?>
-    <?php echo '<!-- Header loaded -->'; ?>
-</head>
-
-<body <?php body_class(); ?>>
+<?php wp_footer(); ?>
