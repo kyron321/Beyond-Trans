e6cde024080d1e6ac453ddb60be62f9db4316107
Kyron <tfkyron@gmail.com>
2025-04-02 23:06:49 +0100

    Various changes for topnav build

--- wp-content/themes/beyond-trans/templates/header/top-nav.php
+++ wp-content/themes/beyond-trans/templates/header/top-nav.php
@@ -3,8 +3,8 @@ $genspect_related_websites = get_field('genspect_related_websites', 'option');
 ?>
 
 <?php if ($genspect_related_websites): ?>
-<nav class="container">
-    <?php foreach ($genspect_related_websites as $website):
+    <nav class="top-nav container">
+        <?php foreach ($genspect_related_websites as $website):
             $url = isset($website['website']['url']) ? $website['website']['url'] : '';
             $title = isset($website['website']['title']) ? $website['website']['title'] : '';
             $website_icon = isset($website['website_icon']) ? $website['website_icon'] : '';
@@ -12,10 +12,10 @@ $genspect_related_websites = get_field('genspect_related_websites', 'option');
             // Get the full SVG code using the ID
             $svg_code = $website_icon ? get_field('full_svg_code', $website_icon) : '';
         ?>
-    <a href="<?php echo $url; ?>" target="_blank" class="related-website">
-        <span class="website-icon"><?php echo $svg_code; ?></span>
-        <span class="website-title"><?php echo $title; ?></span>
-    </a>
-    <?php endforeach; ?>
-</nav>
+            <a href="<?php echo $url; ?>" target="_blank" class="top-nav__related-website">
+                <span class="top-nav__related-website__website-icon"><?php echo $svg_code; ?></span>
+                <span class="top-nav__related-website__website-title"><?php echo $title; ?></span>
+            </a>
+        <?php endforeach; ?>
+    </nav>
 <?php endif; ?>
