ba6dc788ae2a50a7bd3d09fb1e81cb182f0926a1
Kyron <tfkyron@gmail.com>
2025-04-05 16:51:20 +0100

    Changes to banner blocks

--- wp-content/themes/beyond-trans/blocks/banner/banner.php
+++ wp-content/themes/beyond-trans/blocks/banner/banner.php
@@ -5,37 +5,46 @@ $title = get_field('title');
 $sub_title = get_field('sub_title');
 $cta = get_field('cta');
 $cta_two = get_field('cta_two');
+
+// Get block position 
+$position = isset($block['attrs']['position']) ? $block['attrs']['position'] : null;
+$is_first_block = ($position === 0);
+$loading_type = $is_first_block ? 'eager' : 'lazy';
 ?>
 
-<section class="banner-block">
+<section class="banner">
     <?php if ($image): ?>
-    <div class="banner-image">
-        <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>">
-    </div>
+        <div class="banner__image">
+            <img
+                src="<?= $image['url']; ?>"
+                alt="<?= get_alt_text($image); ?>"
+                loading="<?= $loading_type; ?>">
+            </img>
+        </div>
     <?php endif; ?>
 
-    <div class="banner-content">
+    <div class="banner__content container">
         <?php if ($title): ?>
-        <h1 class="banner-title"><?php echo esc_html($title); ?></h1>
+            <h1 class="banner__content__title"><?= $title; ?></h1>
         <?php endif; ?>
 
         <?php if ($sub_title): ?>
-        <p class="banner-sub-title"><?php echo esc_html($sub_title); ?></p>
+            <p class="banner__content__subtitle"><?= $sub_title; ?></p>
         <?php endif; ?>
 
-        <div class="banner-cta">
+        <div class="banner__content__ctas">
             <?php if ($cta): ?>
-            <a href="<?php echo esc_url($cta['url']); ?>" class="btn btn-secondary"
-                target="<?php echo esc_attr($cta['target']); ?>">
-                <?php echo esc_html($cta['title']); ?>
-            </a>
+                <a href="<?= $cta['url']; ?>" class="btn btn-secondary"
+                    target="<?= $cta['target'] ?: '_blank'; ?>">
+                    <?= $cta['title']; ?>
+                </a>
             <?php endif; ?>
 
             <?php if ($cta_two): ?>
-            <a href="<?php echo esc_url($cta_two['url']); ?>" class="btn btn-white"
-                target="<?php echo esc_attr($cta_two['target']); ?>">
-                <?php echo esc_html($cta_two['title']); ?>
-            </a>
+                <a href="<?= $cta_two['url']; ?>" class="btn btn-white"
+                    target="<?= $cta_two['target'] ?: '_blank'; ?>">
+                    <?= $cta_two['title']; ?>
+                </a>
             <?php endif; ?>
         </div>
     </div>
